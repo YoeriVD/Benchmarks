db02a39fc4d14b72c3dc44378eb519e63d2dab2a
yoerivd <yoeri.vandamme@gmail.com>
2024-01-09 14:31:26 +0100

    boxing example

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<BenchmarkStructs>();
+
+public class BenchmarkStructs
+{
+    private readonly DoStuff _stuff = new();
+
+    [Benchmark]
+    public void BenchmarkInterface() => RunAsInterface(_stuff);
+
+    [Benchmark]
+    public void BenchmarkStruct() => RunAsStruct(_stuff);
+
+    private static void RunAsInterface(IDoStuff stuff) => stuff.Do();
+    private static void RunAsStruct(DoStuff stuff) => stuff.Do();
+}
+
+internal struct DoStuff : IDoStuff
+{
+    public int Do() => 1;
+}
+
+internal interface IDoStuff
+{
+    int Do();
+}
